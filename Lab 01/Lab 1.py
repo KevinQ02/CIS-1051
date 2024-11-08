33338b98d15a5217586990bf77fa25b4c9810fc5
Guillermo Quiroz <129895278+KevinQ02@users.noreply.github.com>
2024-11-07 19:43:52 -0500

    Add files via upload

---
+++ Lab 01/Lab 1.py
@@ -0,0 +1,12 @@
+#part 1
+print("Yann Lecun is a French compiuter scientist born July 8th 1960. The fields he works in primarly are machiinne learning, computer vision, mobile robotics and computatiional nnueroscience. .LeCun is also a professor at NYU")
+text = input("Have you heard of Yann LeCun before?:")
+print("They are pretty cool aren't they.")
+
+    #part 2
+print("     *  _____________ *  *   ")
+print("   *    |           |  *   * ")
+print(" *  * __|           |__   * ")
+print("    *  (   o <  o    )  *  ")
+print("  *   (      O        )    *")
+print("*  * (       O         ) * ")
